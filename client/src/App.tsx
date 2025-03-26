e4d490e79150287b03cdb877249dc83079f20e24
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-26 17:27:03 +0530

    sign in page styled

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,20 @@
 import './App.css'
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import MainPage from './pages/main'
+import SignIn from './pages/sign-in'
+import Room from './pages/room'
 
 function App() {
 
 
   return (
-    <>
-    
-    </>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<MainPage/>}/>
+      <Route path='/sign-in' element={<SignIn/>}/>
+      <Route path='/room' element={<Room/>}/>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/pages/sign-in/index.tsx
+++ client/src/pages/sign-in/index.tsx
@@ -1,8 +1,20 @@
 
 const SignIn = () => {
+  
   return (
-    <div>
-      signin page
+    <div className="sign-in">
+      <h1>Welcome to Audiora</h1>
+      <form>
+        <div>
+          <label>Username:</label>
+          <input type="text" required />
+        </div>
+        <div>
+          <label>Name:</label>
+          <input type="text" required />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
     </div>
   )
 }
