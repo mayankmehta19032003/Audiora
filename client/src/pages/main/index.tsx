d582d34aa1c959ae7dcfde5cb971732803179ae2
Mayank Mehta <mayankmehta880@gmail.com>
2025-03-28 03:08:30 +0530

    add styling for main page

--- client/src/pages/main/index.tsx
+++ client/src/pages/main/index.tsx
@@ -1,16 +1,26 @@
-import { StreamVideo } from "@stream-io/video-react-sdk"
-import { useUser } from "../../user-context"
+import { StreamVideo } from "@stream-io/video-react-sdk";
+import { useUser } from "../../user-context";
 import { Navigate } from "react-router-dom";
 
 const MainPage = () => {
+  const { client,user } = useUser();
 
-  const {client} = useUser();
-  if(!client) return <Navigate to="/sign-in"/>
-  return (
-    <StreamVideo client={client}>
-      Main page
-    </StreamVideo>
-  )
-}
+  const createRoom = ()=>{
 
-export default MainPage
+  };
+
+  if (!client) return <Navigate to="/sign-in" />;
+  return <StreamVideo client={client}>
+    <div className="home">
+      <h1> Welcome, {user?.name}</h1>
+      <div className="form">
+        <h2>Create Your Own Room</h2>
+        <input placeholder="Room Name..."/>
+        <input placeholder="Room Description..."/>
+        <button onClick={createRoom}>Create a Room</button>
+      </div>
+    </div>
+  </StreamVideo>;
+};
+
+export default MainPage;
